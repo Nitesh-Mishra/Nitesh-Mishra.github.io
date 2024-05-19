cc43406053372d8c5771a3535ec9f4a80e8ce593
niteshm@hashtagloyalty.com <niteshm@hashtagloyalty.com>
2024-05-19 13:20:20 +0530

    Initial commit with base structure

---
+++ javascript/index.js
@@ -0,0 +1,10 @@
+$(function(){
+  $("#header").load("header.html");
+  $("#home").load("home.html");
+  $("#experiences").load("experiences.html");
+  $("#skills").load("skills.html");
+  // $("#projects").load("projects.html");
+  $("#educations").load("educations.html");
+  $("#publications").load("publications.html");
+  $("#footer").load("footer.html");
+});
